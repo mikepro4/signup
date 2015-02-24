0e11e2bcd8c48bb055098140a8b3fd612651228b
Alexandre Pestana <alexandre@compstak.com>
2015-02-24 17:09:15 -0500

    Changed baseUrl to bower_components

--- app/assets/javascripts/requireconfig.js
+++ app/assets/javascripts/requireconfig.js
@@ -1,25 +1,26 @@
 require.config({
+    baseUrl : '/bower_components/',
     paths: {
-        signup: 'assets/javascripts',
-        actions: 'assets/javascripts/actions',
-        components: 'assets/javascripts/components',
-        constants: 'assets/javascripts/constants',
-        dispatcher: 'assets/javascripts/dispatcher',
-        mixins: 'assets/javascripts/mixins',
-        stores: 'assets/javascripts/stores',
+        signup: '../assets/javascripts',
+        actions: '../assets/javascripts/actions',
+        components: '../assets/javascripts/components',
+        constants: '../assets/javascripts/constants',
+        dispatcher: '../assets/javascripts/dispatcher',
+        mixins: '../assets/javascripts/mixins',
+        stores: '../assets/javascripts/stores',
 
-        jquery: 'bower_components/jquery/dist/jquery',
-        underscore: 'bower_components/underscore/underscore',
-        text: 'bower_components/requirejs-text/text',
+        jquery: 'jquery/dist/jquery',
+        underscore: 'underscore/underscore',
+        text: 'requirejs-text/text',
 
         // react
-        react: 'bower_components/react/react-with-addons',
-        'react-router': 'bower_components/react-router/build/global/ReactRouter',
-        jsx: 'bower_components/jsx-requirejs-plugin/js/jsx',
-        JSXTransformer: 'bower_components/jsx-requirejs-plugin/js/JSXTransformer',
-        flux: 'bower_components/flux/dist/Flux',
-        'es6-promise': 'bower_components/es6-promise-polyfill/promise',
-        eventemitter: 'bower_components/eventemitter2/lib/eventemitter2'
+        react: 'react/react-with-addons',
+        'react-router': 'react-router/build/global/ReactRouter',
+        jsx: 'jsx-requirejs-plugin/js/jsx',
+        JSXTransformer: 'jsx-requirejs-plugin/js/JSXTransformer',
+        flux: 'flux/dist/Flux',
+        'es6-promise': 'es6-promise-polyfill/promise',
+        eventemitter: 'eventemitter2/lib/eventemitter2'
     },
     shim: {
         underscore : {
