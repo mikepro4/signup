349a9467dc851151b7636d6f9145db584e469e71
Alexandre Pestana <alexandre@compstak.com>
2015-03-24 17:54:44 -0400

    ie9 fix

--- app/assets/javascripts/application.jsx
+++ app/assets/javascripts/application.jsx
@@ -98,6 +98,10 @@ define([
       React.render(<Handler/>, document.body);
     });
   } else {
+      // Fix for ie9 - http://tosbourn.com/a-fix-for-window-location-origin-in-internet-explorer/
+      if (!window.location.origin) {
+          window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+      }
       if (window.location.pathname !== '/') {
           window.location.href = window.location.origin + '/#' + window.location.pathname;
       }
